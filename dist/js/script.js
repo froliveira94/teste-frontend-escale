ef1ed884bec642ffca05a8b49dbcf02764cbe96d
Felipe Rodrigues <fr.oliveira94@gmail.com>
2017-02-25 14:44:22 -0300

    adjust fonts

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,11 +8,26 @@ var App = {
         .done(function(data){
             console.log(data);
             $('#avatarUser').html("<img src='"+data.avatar_url+"'class='responsive-img'>");
+            $('#userInfoName').html(data.name);
+            $('#userInfoData').html("<li>" + "<i class='material-icons left'>perm_identity</i><span class='left'>" + data.login + "</span></li>" + "<li>" + "<i class='material-icons left'>work</i><span class='left'>" + data.company + "</span></li>" + "<li>" + "<i class='material-icons left'>email</i><span class='left'>" + data.email + "</span></li>")
         })
         .fail(function() {
             console.log( "Ocorreu algum erro!" );
-        });
-      return dataUser;       
+        });    
+    },
+
+    renderUserRepo : function(userUrlRepo) {
+     var url =  "https://api.github.com/users/" + userUrlRepo; 
+     var dataUserRepo =  $.getJSON( url, function() {})
+        .done(function(data){
+            console.log(data);
+            $.each( data, function(key, val) {
+               $('#userRepos').html("<div class='col s12 m6'><div class='card blue-grey darken-1'><div class='card-content white-text'><span class='card-title'>"+val.name+"</span></div></div></div>");
+            });
+        })
+        .fail(function() {
+            console.log( "Ocorreu algum erro!" );
+        });    
     },
 
     parallax : function(){
@@ -23,5 +38,6 @@ var App = {
 $(document).ready(function(){
      App.parallax();
      App.renderUserInfo('wilfernandesjr');
+     App.renderUserRepo('wilfernandesjr/starred');
 });
         
